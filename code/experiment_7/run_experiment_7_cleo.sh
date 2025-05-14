066988a5b98c1fa7a7dad264ba7410fcddcf4099
Jacob Morra <jacob.morra@duke.edu>
2025-05-13 21:21:48 -0400

    ...

--- code/experiment_7/run_experiment_7_cleo.sh
+++ code/experiment_7/run_experiment_7_cleo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=nlp4neuro          # Job name
-#SBATCH --output=logs/nlp4neuro_exp1_%j.out # Standard output file (%j will be replaced by the job ID)
-#SBATCH --error=logs/nlp4neuro_exp1_%j.err  # Standard error file
+#SBATCH --output=logs/nlp4neuro_exp7cleo_%j.out # Standard output file (%j will be replaced by the job ID)
+#SBATCH --error=logs/nlp4neuro_exp7cleo_%j.err  # Standard error file
 #SBATCH --time=72:00:00                    # Max run time (adjust as needed)
 #SBATCH --mem=64G                         # Memory request per node
 #SBATCH --cpus-per-task=8                  # Number of CPU cores per task
