b42b8d7453ebd25c983d60f27b208747e227e4a0
Jacob Morra <jacobjmorra@gmail.com>
2025-06-18 14:49:21 -0400

    ...

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 from pathlib import Path
 import yaml, pkg_resources  # built-in yaml if you pip-installed pyyaml
 
-_cfg_file = Path(__file__).parents[1] / "config.yaml"
-if not _cfg_file.exists():                         # CI fallback
-    _cfg_file = _cfg_file.with_name("config_example.yaml")
+_cfg_file = Path(__file__).parents[0] / "config.yaml"
+# if not _cfg_file.exists():                         # CI fallback
+#     _cfg_file = _cfg_file.with_name("config_example.yaml")
 
 with _cfg_file.open() as f:
     _cfg = yaml.safe_load(f)
